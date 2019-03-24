7ac86a0556faab6c05c9258965f348ccf7ce1b2e
Harish Kumar <42680074+harishsuthar11@users.noreply.github.com>
2019-03-24 22:02:37 +0530

    Stack.java

---
+++ StackLinearList.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class StackLinearList<X> implements Stack<X> {
+
+    private ArrayList<X> list;
+
+    public StackLinearList() {
+        list = new ArrayList<X>();
+    }
+
+
+    public void push(X item) {
+        list.add(item);
+    }
+
+
+    public X pop() {
+        if(list.size()==0)
+            throw new IllegalStateException("empty");
+        return list.remove(list.size()-1);
+    }
+
+
+    public boolean isEmpty(X item) {
+        return list.isEmpty(item);
+    }
+
+
+    public X peek(X item) {
+        try {
+            while (true) {
+                    if (item == pop())
+                        return item;
+                }
+
+            }
+        catch (Exception e){
+            System.out.println("Item not found ");
+            return null;
+        }
+
+    }
+
+
+    public int size() {
+        return list.size();
+    }
+}
+
+    
